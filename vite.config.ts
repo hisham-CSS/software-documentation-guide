2d12fadadf526833e88c6f3fad9ad540d2b6b5d9
Hisham Ata <hisham@catsplatstudios.com>
2025-11-07 16:24:25 -0500

    updated more dependances.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import fs from "node:fs";
 import path from "path";
 import { defineConfig } from "vite";
+
 const plugins = [react(), tailwindcss()];
 
 export default defineConfig({
